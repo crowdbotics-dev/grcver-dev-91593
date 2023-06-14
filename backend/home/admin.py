29a2099b237c873da7956a13590c2af9259058dd
crowdbotics-dev <engineering@crowdbotics.com>
2023-06-14 17:12:55 +0000

    Update models

--- backend/home/admin.py
+++ backend/home/admin.py
@@ -1,3 +1,5 @@
 from django.contrib import admin
+from .models import Dcertt
+admin.site.register(Dcertt)
 
 # Register your models here.
--- backend/home/models.py
+++ backend/home/models.py
@@ -1,1 +1,5 @@
+from django.conf import settings
 from django.db import models
+class Dcertt(models.Model):
+    'Generated Model'
+    nmcm = models.BigIntegerField()
